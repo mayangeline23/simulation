27bca376115da6234d9ed5a3880e4fd58de6718c
May Angeline M. Regaspi <145741270+mayangeline23@users.noreply.github.com>
2024-12-14 11:40:45 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("Heart Disease Prediction")
+# Set the page title and layout
+st.set_page_config(page_title="Health Disease Prediction - Final Project", layout="centered")
+
+# Welcome Message 
+st.title("Health Disease Prediction🩺")
+
+# Introduction
+st.write("""
+This project focuses on **Health Disease**, showcasing our application of modeling and simulation techniques. 
+We've worked together to explore synthetic health data, perform statistical analysis, and develop predictive models to assess various health risks.
+
+This project is the culmination of our group's dedication and effort for the **CSEC 413 Modeling and Simulation** course, and we're excited to share our work with you.
+""")
+
+# Why Health Disease Prediction
+st.write("""
+💡 **Why Health Disease Prediction?**  
+Health diseases continue to be a major concern globally. By leveraging data modeling and simulation, our aim is to provide insights that contribute to understanding and managing health risks effectively.
+""")
+
+# Thank you message
+st.write("Thank you for taking the time to view our work. Happy exploring! 🤓")
